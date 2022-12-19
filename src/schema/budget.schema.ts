f0824bae298d24d12f5d5ce58e0c72c6876d13b8
Inu John <inuoshios@gmail.com>
2022-12-19 21:02:32 +0100

    chore: fixed typo in schema

--- src/schema/budget.schema.ts
+++ src/schema/budget.schema.ts
@@ -1,74 +1,72 @@
 import { z } from 'zod';
 
 export const createBudgetSchema = z.object({
-    body: z.object({
-        description: z.string().optional(),
-        amount: z.number({
-            required_error: "amount cannot be empty"
-        }),
+  body: z.object({
+    description: z.string().optional(),
+    amount: z.number({
+      required_error: 'amount cannot be empty',
+    }),
 
-        month: z.enum([
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ]),
-
-    })
+    month: z.enum([
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]),
+  }),
 });
 
 export const getSingleBudgetSchema = z.object({
-    params: z.object({
-        budgetId: z.string({
-            required_error: "budge"
-        })
-    })
+  params: z.object({
+    budgetId: z.string({
+      required_error: 'budgetId is required',
+    }),
+  }),
 });
 
 export const deleteBudgetSchema = z.object({
-    params: z.object({
-        budgetId: z.string({
-            required_error: "budge"
-        })
-    })
+  params: z.object({
+    budgetId: z.string({
+      required_error: 'budgetId is required',
+    }),
+  }),
 });
 
 export const updateBudgetSchema = z.object({
-    body: z.object({
-        description: z.string().optional(),
-        amount: z.number().optional(),
+  body: z.object({
+    description: z.string().optional(),
+    amount: z.number().optional(),
 
-        month: z.enum([
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ]),
+    month: z.enum([
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]),
 
-        userId: z.string().optional(),
-
+    userId: z.string().optional(),
+  }),
+  params: z.object({
+    budgetId: z.string({
+      required_error: 'budgetId is required',
     }),
-    params: z.object({
-        budgetId: z.string({
-            required_error: "budgetId is required"
-        })
-    })
+  }),
 });
 
 export type createBudgetInput = z.TypeOf<typeof createBudgetSchema>;
